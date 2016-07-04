a5182e22de05cb801a763b0699e4a7c908350104
JianwuGao <Dreamtale.jg@gmail.com>
2016-07-04 21:43:28 +0800

    update webview config

--- app/src/main/java/com/performance/liferecord/activity/PostDetailActivity.java
+++ app/src/main/java/com/performance/liferecord/activity/PostDetailActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -15,6 +17,7 @@ import com.performance.liferecord.model.GankData;
 
 public class PostDetailActivity extends AppCompatActivity {
 
+    private static final String TAG = "PostDetailActivity";
     private WebView mWebView;
     private String postUrl;
 
@@ -23,41 +26,38 @@ public class PostDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
 
-        setContentView(R.layout.activity_post_detail);
         getWindow().setFeatureInt(Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
 
-        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView = new WebView(this);;
         final Activity MyActivity = this;
         WebSettings webSettings = null;
-        if (mWebView != null) {
-            webSettings = mWebView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-            webSettings.setSupportZoom(true);
-            webSettings.setSupportMultipleWindows(true);
-            mWebView.setWebViewClient(new WebViewClient());
-            mWebView.setWebChromeClient(new WebChromeClient() {
-                @Override
-                public void onProgressChanged(WebView view, int newProgress) {
-                    super.onProgressChanged(view, newProgress);
-                    MyActivity.setTitle(getString(R.string.loading));
-                    MyActivity.setProgress(newProgress * 100);
-                    // Return the app name after finish loading
-                    if (newProgress == 100)
-                        MyActivity.setTitle(R.string.app_name);
-                }
+        webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setSupportZoom(true);
+        webSettings.setSupportMultipleWindows(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                MyActivity.setTitle(getString(R.string.loading));
+                MyActivity.setProgress(newProgress * 100);
+                // Return the app name after finish loading
+                if (newProgress == 100)
+                    MyActivity.setTitle(R.string.app_name);
+            }
 
-            });
-        }
+        });
 
         getData();
-
     }
 
     private void getData() {
         Intent intent = getIntent();
         postUrl = intent.getStringExtra(GankData.POST_URL);
         mWebView.loadUrl(postUrl);
+        setContentView(mWebView);
     }
 
     @Override
@@ -73,11 +73,13 @@ public class PostDetailActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-            return;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.v(TAG,"onKeyDown");
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack();// 返回前一个页面
+            return true;
         }
-        super.onBackPressed();
+
+        return super.onKeyDown(keyCode, event);
     }
 }
